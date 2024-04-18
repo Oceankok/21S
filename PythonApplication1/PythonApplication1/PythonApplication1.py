994d3b42b10cc5de3255106111d0840a0a80b304
oceankok <asdfghdebdo@gmail.com>
2024-04-19 03:52:46 +0900

    인터페이스

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+win = Tk()
+win.geometry("500x700")
+win.title("Health_abata")
+win.option_add("*Font","Arial 25")
+
+btn = Button(win, text="Start")
+btn.pack()
+win.mainloop()
